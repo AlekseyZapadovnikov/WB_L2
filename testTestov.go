f97babd523e2b681f30da2c7e47e356a8348c0ac
Aleksey Zapadovnikov <zapadovnikov145@gmail.com>
2025-10-20 16:20:48 +0300

    ...

--- testTestov.go
+++ testTestov.go
@@ -1,34 +1,11 @@
-package l2
+package main
 
-import(
+import (
 	"fmt"
+	"os"
 )
 
 
-func UnnamedReturn() int {
-  num := 100
-
-  defer func(num int) {
-    fmt.Printf("num value in the 1st defer `before`: %d\n", num)
-    num = 777
-    fmt.Printf("num value in the 1st after `after`: %d\n", num)
-  }(num)
-
-  num = 200
-
-  defer func() {
-    fmt.Printf("num value in the 2nd defer `before`: %d\n", num)
-    num = 300
-    fmt.Printf("num value in the 2nd defer `after`: %d\n", num)
-  }()
-
-  fmt.Printf("num value before function exiting: %d\n", num)
-  return num
+func main() {
+  fmt.Println(os.Getpagesize())
 }
-
-// num value before function exiting: 200
-// num value in the 2nd defer `before`: 200
-// num value in the 2nd defer `after`: 300
-// num value in the 1st defer `before`: 100
-// num value in the 1st after `after`: 777
-// Func result: 200
